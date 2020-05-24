d21793871663556d35030a2ee8f6fad87b9d7ae1
Rabbil Hasan <33941621+rupomsoft@users.noreply.github.com>
2020-05-25 04:11:40 +0600

    Front End on Going , General Laravel Requirements on going
    Front End on Going , General Laravel Requirements on going

--- Check_Requirements.php
+++ Check_Requirements.php
@@ -1,17 +1,54 @@
 <?php
-
-
 include 'Laravel_Requirements.php';
 
-
-?>
-
-<form>
-<select>
-<?php for($x = 0; $x <$VersionArrLength; $x++){ ?>
-	<option><?php echo $LaravelVersions[$x]; ?></option>
-<?php	
+if(isset($_POST['submit'])){
+    $GeneralArray=GeneralConfigCheck();
 }
+
+
 ?>
-</select>
-</form>
+
+
+<html lang="en">
+<head>
+    <title>Laravel Server Requirements </title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+
+
+<div class="container mt-5">
+    <div class="row">
+        <div class="col-md-4">
+            <form action="" method="post">
+                <select name="LaravelVersion" class="form-control">
+                    <?php for($x = 0; $x <$VersionArrLength; $x++){ ?>
+                        <option><?php echo $LaravelVersions[$x]; ?></option>
+                        <?php
+                    }
+                    ?>
+                </select>
+                <input class="btn btn-danger btn-block mt-4" type="submit" name="submit" value="Check"/>
+            </form>
+        </div>
+        <div class="col-md-8">
+            <table class="table">
+                <tr><td>BC Math Extension</td><td></td></tr>
+                <tr><td>C-Type Extension</td><td></td></tr>
+                <tr><td>File Info Extension</td><td></td></tr>
+                <tr><td>JSON Support</td><td></td></tr>
+                <tr><td>MB String Extension</td><td></td></tr>
+                <tr><td>Open SSL</td><td></td></tr>
+                <tr><td>Tokenizer Support</td><td></td></tr>
+                <tr><td>XML Extension</td><td></td>
+                </tr><tr><td>PDO Extension</td><td></td></tr>
+            </table>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
+
+
+
--- Laravel_Requirements.php
+++ Laravel_Requirements.php
@@ -1,6 +1,4 @@
 <?php
-
-
 $LaravelVersions=array(
 "Laravel 7x",
 "Laravel 6x",
@@ -8,12 +6,46 @@ $LaravelVersions=array(
 "Laravel 5.7",
 "Laravel 5.6",
 "Laravel 5.5",
-"Laravel 5.4",
-"Laravel 5.3",
-"Laravel 5.2",
-"Laravel 5.1",
-"Laravel 5.0",
 );
 $VersionArrLength = count($LaravelVersions);
+
+$LaravelGeneral=array(
+    'bcmath'=>true,
+    'ctype'=>true,
+    'fileInfo'=>true,
+    'json'=>true,
+    'mbString'=>true,
+    'openSSL'=>true,
+    'tokenizer'=>true,
+    'xml'=>true,
+    'pdo'=>true
+);
+
+
+function GeneralConfigCheck(){
+    $bcmath=extension_loaded('bcmath');
+    $ctype=extension_loaded('ctype');
+    $fileInfo=extension_loaded('Fileinfo');
+    $json=extension_loaded('json');
+    $mbString=extension_loaded('mbstring');
+    $openSSL=extension_loaded('openssl');
+    $tokenizer=extension_loaded('tokenizer');
+    $xml=extension_loaded('xml');
+    $pdo=defined('PDO::ATTR_DRIVER_NAME');
+    $GeneralArray=array(
+        'bcmath'=>$bcmath,
+        'ctype'=>$ctype,
+        'fileInfo'=>$fileInfo,
+        'json'=>$json,
+        'mbString'=>$mbString,
+        'openSSL'=>$openSSL,
+        'tokenizer'=>$tokenizer,
+        'xml'=>$xml,
+        'pdo'=>$pdo
+        );
+    return $GeneralArray;
+
+
+}
 
 
